d3ac20ac0d4afd540b6b762a55277e04cbd7f60b
samuelheroy <sam.heroy@gmail.com>
2017-03-23 18:41:43 -0400

    Update make_ints_agree.m

--- make_ints_agree.m
+++ make_ints_agree.m
@@ -1,34 +1,23 @@
-x_1=sizes(1);
-x_2=sizes(2);
-x_3=sizes(3);
-x_4=sizes(4);
-x_5=sizes(5);
-x=[x_1; x_2; x_3; x_4; x_5]';
-x1=[x'; zeros(20,1)];
-x2=[zeros(5,1); x'; zeros(15,1)];
-x3=[zeros(10,1); x'; zeros(10,1)];
-x4=[zeros(15,1); x'; zeros(5,1)];
-x5=[zeros(20,1); x'];
-
-
-C=[ones(length(x)*5,1) x1 x2 x3 x4 x5];
-
-
-
-d=[q_2'; q_4'; q_6'; q_7'; q_85'];
-
+function [a]=make_ints_agree(X,Y,show_plot)
+%This code is used for simple linear regression on multiple data sets, wherein each line of best fit is constrained to have the same y-intercept. This is useful for finding percolation thresholds, among other things.
+%X is an m x n matrix, where each column represents the m observations of each of the n variates.
+%Y is an m x matrix 
+%If show_plot==1, the plots the resulting regression lines and data.
+[m,n]=size(X); %m is the number of data points per variate, n is the number of variates. For this implementation, the number of observations per variate must be the same
+C=[ones(m*n,1) zeros(m*n,n)]; %this will be an m*n x n+1 matrix (we're going to solve Ca=d)
+d=zeros(m*n,1); %this is an m x n vector (just reogranizing the output matrix)
+for i=1:n
+  C(i+1,(i-1)*m+1:m*i)=X(1:m,i);
+  d((i-1)*m+1:m*i)=Y(1:m,i);
+end
 a=lsqlin(C,d,[],[],[],[],[],[],[],options);
-plot([0 x],a(2)*[0 x]+a(1),'r')
-hold on;
-plot([0 x],a(3)*[0 x]+a(1),'b')
-plot([0 x],a(4)*[0 x]+a(1),'g')
-plot([0 x],a(5)*[0 x]+a(1),'m')
-plot([0 x],a(6)*[0 x]+a(1),'c')
-plot(x,q_2,'*')
-plot(x,q_4,'*')
-plot(x,q_6,'*')
-plot(x,q_7,'*')
-plot(x,q_85,'*')
 
-
-
+if show_plot==1
+  hold on;
+  for i=1:n
+    plot([0 X(i,1:m)],a(1)+a(i+1)*[0 X(i,1:m)])
+  end
+  for i=1:n
+    plot(X(1:m,i),Y(1:m,i),'*')
+  end
+end
